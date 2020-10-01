c27a47f638b9051dd4003af476dd17a36a430ad2
hiteshv01 <67196487+hiteshv01@users.noreply.github.com>
2020-10-01 10:49:39 +0530

    Create linkedlist.cpp

---
+++ linkedlist.cpp
@@ -0,0 +1,29 @@
+ // https://www.facebook.com/hitesh.vishnoi.146/posts/342179847062773
+ // subscribe by Hitesh Vishnoi
+ #include <iostream>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+};
+
+class linked_list
+{
+private:
+    node *head,*tail;
+public:
+    linked_list()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+};
+
+int main()
+{
+    linked_list a;
+    return 0;
+}
