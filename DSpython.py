ed7434e545dd286cf01d48347907e6735739497d
avinashupadhyay <68536058+avinashupadhyay@users.noreply.github.com>
2020-10-02 12:09:37 +0530

    Create DSpython.py

---
+++ DSpython.py
@@ -0,0 +1,24 @@
+#https://www.facebook.com/KrishAnshuAvinash/posts/1298375203845472
+#subscribed by Avinash Upadhyay
+
+
+#!/usr/bin/python
+
+import smtplib
+
+sender = 'from@fromdomain.com'
+receivers = ['to@todomain.com']
+
+message = """From: From Person <from@fromdomain.com>
+To: To Person <to@todomain.com>
+Subject: SMTP e-mail test
+
+This is a test e-mail message.
+"""
+
+try:
+   smtpObj = smtplib.SMTP('localhost')
+   smtpObj.sendmail(sender, receivers, message)         
+   print "Successfully sent email"
+except SMTPException:
+   print "Error: unable to send email"
